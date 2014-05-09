beb0c91126ec03efd881e986051ed8fc8a9a2ed8
Renato Zannon <renato@rrsz.com.br>
2014-05-09 00:46:19 -0300

    Remove a few unnecessary heap allocations

--- conn.rs
+++ conn.rs
@@ -409,7 +409,7 @@ impl MyConn {
         try_io!(writer.write_le_u32(client_flags));
         try_io!(writer.write_le_u32(0u32));
         try_io!(writer.write_u8(consts::UTF8_GENERAL_CI));
-        try_io!(writer.write(~[0u8, ..23]));
+        try_io!(writer.write([0u8, ..23]));
         try_io!(writer.write_str(self.opts.get_user()));
         try_io!(writer.write_u8(0u8));
         try_io!(writer.write_u8(scramble_buf_len as u8));
@@ -1100,7 +1100,7 @@ mod test {
                                           user: Some("root".to_owned()),
                                           ..Default::default()}).unwrap();
         let mut stmt = conn.prepare("SELECT ?, ?, ?, ?, ?").unwrap();
-        let params = ~[Int(42), Bytes(vec!(104u8, 101u8, 108u8, 108u8, 111u8, 111u8)), Float(1.618), NULL, Int(1)];
+        let params = [Int(42), Bytes(vec!(104u8, 101u8, 108u8, 108u8, 111u8, 111u8)), Float(1.618), NULL, Int(1)];
         bench.iter(|| { stmt.execute(params); })
     }
 
