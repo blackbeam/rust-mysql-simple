3bd03745ebcd1942d3428927a47b952e180dfcbe
Ryan Piper <ryan.piper@displaysystemsintl.com>
2017-02-16 10:31:50 -0600

    Reverted to where the json mods were in there own folder. Made rustc a target dependency.

--- src/lib.rs
+++ src/lib.rs
@@ -149,9 +149,9 @@ extern crate bufstream;
 extern crate fnv;
 pub extern crate chrono;
 extern crate twox_hash;
-#[cfg(not(feature ="serde_integration"))]
-pub extern crate rustc_serialize;
-#[cfg(feature ="serde_integration")]
+#[cfg(not(feature="serde_integration"))]
+extern crate rustc_serialize;
+#[cfg(feature="serde_integration")]
 pub extern crate serde;
 #[cfg(feature ="serde_integration")]
 pub extern crate serde_json;
