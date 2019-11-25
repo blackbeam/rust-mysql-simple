8c9006c3e96c573b7d992013cd7acb4f5226d087
Anatoly I <blackbeam@users.noreply.github.com>
2019-11-25 10:01:19 +0300

    Reexport ColumnIndex trait (fix #194)

--- src/lib.rs
+++ src/lib.rs
@@ -178,6 +178,8 @@ pub mod prelude {
     #[doc(inline)]
     pub use crate::conn::GenericConnection;
     #[doc(inline)]
+    pub use crate::myc::row::ColumnIndex;
+    #[doc(inline)]
     pub use crate::myc::row::convert::FromRow;
     #[doc(inline)]
     pub use crate::myc::value::convert::{ConvIr, FromValue, ToValue};
