3976807c8fd11b06769caa71b9c7068b1b59e1bc
Anatoly Ikorsky <aikorsky@gmail.com>
2020-06-27 18:01:25 +0300

    Replace deprecated net2 crate in favor of the socket2 crate (fix #231)

--- src/io/tcp.rs
+++ src/io/tcp.rs
@@ -6,25 +6,13 @@
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
 
-use net2::{TcpBuilder, TcpStreamExt};
-#[cfg(unix)]
-use nix::{
-    errno::Errno,
-    poll::{self, PollFlags},
-    sys::socket,
-};
-#[cfg(target_os = "windows")]
-use winapi::um::winsock2::*;
+use socket2::{Domain, Socket, Type};
 
-#[cfg(unix)]
-use std::os::unix::prelude::*;
 use std::{
-    io, mem,
+    io,
     net::{SocketAddr, TcpStream, ToSocketAddrs},
     time::Duration,
 };
-#[cfg(target_os = "windows")]
-use std::{os::raw::*, os::windows::prelude::*, ptr};
 
 pub struct MyTcpBuilder<T> {
     address: T,
@@ -102,169 +90,30 @@ impl<T: ToSocketAddrs> MyTcpBuilder<T> {
             .to_socket_addrs()?
             .fold(Err(err), |prev, sock_addr| {
                 prev.or_else(|_| {
-                    let builder = if sock_addr.is_ipv4() {
-                        TcpBuilder::new_v4()?
+                    let socket = if sock_addr.is_ipv4() {
+                        Socket::new(Domain::ipv4(), Type::stream(), None)?
                     } else {
-                        TcpBuilder::new_v6()?
+                        Socket::new(Domain::ipv6(), Type::stream(), None)?
                     };
                     if let Some(bind_address) = bind_address {
                         if bind_address.is_ipv4() == sock_addr.is_ipv4() {
-                            builder.bind(bind_address)?;
+                            socket.bind(&bind_address.into())?;
                         }
                     }
                     if let Some(connect_timeout) = connect_timeout {
-                        #[cfg(unix)]
-                        connect_fd_timeout(builder.as_raw_fd(), &sock_addr, connect_timeout)?;
-                        #[cfg(target_os = "windows")]
-                        connect_fd_timeout(builder.as_raw_socket(), &sock_addr, connect_timeout)?;
-                        builder.to_tcp_stream()
+                        socket.connect_timeout(&sock_addr.into(), connect_timeout)?;
                     } else {
-                        builder.connect(sock_addr)
+                        socket.connect(&sock_addr.into())?;
                     }
+                    Ok(socket)
                 })
             })
-            .and_then(|stream| {
-                stream.set_read_timeout(read_timeout)?;
-                stream.set_write_timeout(write_timeout)?;
-                stream.set_keepalive_ms(keepalive_time_ms)?;
-                stream.set_nodelay(nodelay)?;
-                Ok(stream)
+            .and_then(|socket| {
+                socket.set_read_timeout(read_timeout)?;
+                socket.set_write_timeout(write_timeout)?;
+                socket.set_keepalive(keepalive_time_ms.map(|x| Duration::from_millis(x as u64)))?;
+                socket.set_nodelay(nodelay)?;
+                Ok(socket.into_tcp_stream())
             })
     }
-}
-
-#[cfg(unix)]
-fn set_non_blocking(fd: RawFd, non_blocking: bool) -> io::Result<()> {
-    let stream = unsafe { TcpStream::from_raw_fd(fd) };
-    let result = stream.set_nonblocking(non_blocking);
-    mem::forget(stream);
-    result
-}
-
-#[cfg(target_os = "windows")]
-fn set_non_blocking(socket: RawSocket, non_blocking: bool) -> io::Result<()> {
-    let stream = unsafe { TcpStream::from_raw_socket(socket) };
-    let result = stream.set_nonblocking(non_blocking);
-    mem::forget(stream);
-    result
-}
-
-#[cfg(unix)]
-fn connect_fd_timeout(fd: RawFd, sock_addr: &SocketAddr, timeout: Duration) -> io::Result<()> {
-    set_non_blocking(fd, true)?;
-
-    let inet_addr = socket::InetAddr::from_std(sock_addr);
-    let sock_addr = socket::SockAddr::Inet(inet_addr);
-    match socket::connect(fd, &sock_addr) {
-        Ok(_) => (),
-        Err(err) => match err {
-            ::nix::Error::Sys(Errno::EALREADY) | ::nix::Error::Sys(Errno::EINPROGRESS) => (),
-            ::nix::Error::Sys(errno) => return Err(io::Error::from_raw_os_error(errno as i32)),
-            _ => return Err(io::Error::new(io::ErrorKind::Other, err)),
-        },
-    }
-
-    let socket_fd = fd;
-    let mut poll_fds = [poll::PollFd::new(socket_fd, PollFlags::POLLIN)];
-    let timeout_millis = timeout.as_secs() as i32 * 1000;
-
-    let poll_res = poll::poll(&mut poll_fds, timeout_millis);
-
-    let poll_res = poll_res.map_err(|err| match err {
-        ::nix::Error::Sys(errno) => io::Error::from_raw_os_error(errno as i32),
-        _ => io::Error::new(io::ErrorKind::Other, err),
-    })?;
-
-    if poll_res == -1 {
-        return Err(io::Error::last_os_error());
-    }
-
-    if poll_res != 1 {
-        return Err(io::ErrorKind::TimedOut.into());
-    }
-
-    let socket_error_code = socket::getsockopt(socket_fd, socket::sockopt::SocketError);
-
-    let socket_error_code = socket_error_code.map_err(|err| match err {
-        ::nix::Error::Sys(errno) => io::Error::from_raw_os_error(errno as i32),
-        _ => io::Error::new(io::ErrorKind::Other, err),
-    })?;
-
-    if socket_error_code != 0 {
-        return Err(io::Error::from_raw_os_error(socket_error_code));
-    }
-
-    set_non_blocking(fd, false)
-}
-
-#[cfg(target_os = "windows")]
-fn connect_fd_timeout(
-    socket: RawSocket,
-    sock_addr: &SocketAddr,
-    timeout: Duration,
-) -> io::Result<()> {
-    set_non_blocking(socket, true)?;
-    let (name, name_len) = match *sock_addr {
-        SocketAddr::V4(ref a) => (a as *const _ as *const _, mem::size_of_val(a) as c_int),
-        SocketAddr::V6(ref a) => (a as *const _ as *const _, mem::size_of_val(a) as c_int),
-    };
-    let result = unsafe { connect(socket as usize, name, name_len) };
-    if result == SOCKET_ERROR {
-        let err = io::Error::last_os_error();
-        match err.raw_os_error() {
-            Some(WSAEWOULDBLOCK) => {
-                let mut write_fds = fd_set {
-                    fd_count: 1,
-                    fd_array: [0; FD_SETSIZE],
-                };
-                write_fds.fd_array[0] = socket as usize;
-                let mut err_fds = write_fds.clone();
-                let timeout = timeval {
-                    tv_sec: timeout.as_secs() as c_long,
-                    tv_usec: 0,
-                };
-
-                let result =
-                    unsafe { select(0, ptr::null_mut(), &mut write_fds, &mut err_fds, &timeout) };
-
-                if result == 0 {
-                    return Err(io::ErrorKind::TimedOut.into());
-                } else if result == SOCKET_ERROR {
-                    return Err(io::Error::last_os_error());
-                } else {
-                    let mut error = None;
-                    for i in 0..(err_fds.fd_count as usize) {
-                        if err_fds.fd_array[i] == socket as usize {
-                            error = Some(true);
-                        }
-                    }
-                    for i in 0..(write_fds.fd_count as usize) {
-                        if write_fds.fd_array[i] == socket as usize {
-                            error = Some(false);
-                        }
-                    }
-                    match error {
-                        Some(false) => (),
-                        Some(true) => {
-                            let mut opt_val = 0i32;
-                            let mut opt_len = mem::size_of::<i32>() as c_int;
-                            let result = unsafe {
-                                getsockopt(
-                                    socket as usize,
-                                    SOL_SOCKET,
-                                    SO_ERROR,
-                                    &mut opt_val as *mut _ as *mut _,
-                                    &mut opt_len,
-                                )
-                            };
-                            return Err(io::Error::from_raw_os_error(result));
-                        }
-                        None => unreachable!(),
-                    }
-                }
-            }
-            _ => return Err(err),
-        }
-    }
-    set_non_blocking(socket, false)
 }
